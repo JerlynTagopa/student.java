9702bd4af1beec767ef98d6c6fc0c5af7810569b
JerlynTagopa <146070025+JerlynTagopa@users.noreply.github.com>
2023-10-13 09:15:14 +0800

    Add files via upload

---
+++ student.java
@@ -0,0 +1,26 @@
+public class student {
+    public static String name;
+    public static String section;
+    public static String motto;
+    
+    public String getname() {
+        return name;
+    }
+    public void setname(String newname) {
+        name = newname;
+    }
+    
+    public String getsection() {
+        return section;
+    }
+    public void setsection(String newsection) {
+        section = newsection;
+    }
+    
+    public String getmotto() {
+        return motto;
+    }
+    public void setmotto(String newmotto) {
+        motto = newmotto;
+    }
+}
